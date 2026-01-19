7c0d36c247e69c53e53f65febf7f48973c1b297a
Adrian Wartono <adrwartono@gmail.com>
2026-01-19 18:06:18 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
+import { NextRequest } from "next/server";
 
-export default auth((req) => {
+export default auth((req: NextRequest) => {
   if (!req.auth) {
     const url = new URL("/login", req.url);
     return Response.redirect(url);
